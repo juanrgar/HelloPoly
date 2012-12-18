68bb244100892022218e6e3f03e6fe74cc269a23
juanrgar <juanrgar@gmail.com>
2012-12-18 10:49:48 +0100

    Initial Commit

---
+++ HelloPoly/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloPoly
+//
+//  Created by Juan Rafael García Blanco on 12/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ HelloPoly/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  HelloPoly
+//
+//  Created by Juan Rafael García Blanco on 12/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+	
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
